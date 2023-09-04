884b75f3334859361ef9b16e903ad76b53d96058
MariumIrfan2 <mariumirfan7@gmail.com>
2023-09-04 18:06:23 +0500

    project route

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const route = express.Router();
-const { sendResponse } = require("../helper/helper");
-const taskModel = require("../models/taskModel");
-const bcrypt = require("bcryptjs");
 const TaskController = require("../controllers/taskController");
 
 
